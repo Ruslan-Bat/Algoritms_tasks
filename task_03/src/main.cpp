1afb5955a9a59d3082d85e21a6aaaa435fbce710
Ruslan <140762276+Ruslan-Bat@users.noreply.github.com>
2026-06-11 20:18:26 +0000

    03 сделано

--- task_03/src/main.cpp
+++ task_03/src/main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include "func_03.h"
 
-int main() { return 0; }
+int main() {
+	std::string S;
+	if (!(std::cin >> S))
+		return 0;
+
+	std::vector<std::string> res = func(S);
+
+	for (size_t i = 0; i < res.size(); ++i) {
+		if (i) std::cout << ' ';
+		std::cout << res[i];
+	}
+	if (!res.empty()) std::cout << '\n';
+
+	return 0;
+}
