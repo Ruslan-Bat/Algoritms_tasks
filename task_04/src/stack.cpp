e597828d69552a8d8d5c0617b2e2b038834d280b
Ruslan <140762276+Ruslan-Bat@users.noreply.github.com>
2026-06-12 10:52:52 +0000

    04 сделано

--- task_04/src/stack.cpp
+++ task_04/src/stack.cpp
@@ -1,21 +1,27 @@
 #include "stack.hpp"
 
-#include <algorithm>
 
-void Stack::Push(int value) { data_.push(value); }
+void Stack::Push(int value) { data_.push_back(value); }
 
 int Stack::Pop() {
-  auto result = data_.top();
-  data_.pop();
-  return result;
-}
-
-void MinStack::Push(int value) { data_.push_back(value); }
-
-int MinStack::Pop() {
   auto result = data_.back();
   data_.pop_back();
   return result;
 }
 
-int MinStack::GetMin() { return *std::min_element(data_.begin(), data_.end()); }
+void MinStack::Push(int value) { 
+  data_.push_back(value);
+  if (mins_.empty() || value < mins_.back())
+    mins_.push_back(value);
+  else
+    mins_.push_back(mins_.back());
+}
+
+int MinStack::Pop() {
+  auto result = data_.back();
+  data_.pop_back();
+  mins_.pop_back();
+  return result;
+}
+
+int MinStack::GetMin() { return mins_.back();}
--- task_04/src/test.cpp
+++ task_04/src/test.cpp
@@ -1,8 +1,6 @@
 
 #include <gtest/gtest.h>
 
-#include <stack>
-
 #include "stack.hpp"
 
 TEST(StackTest, Simple) {
