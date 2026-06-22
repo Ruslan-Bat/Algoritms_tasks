080ebd5772e5601a65f711e5bf91e14dc866feb6
Ruslan <140762276+Ruslan-Bat@users.noreply.github.com>
2026-06-22 13:37:35 +0000

    Поправил тесты в 08

--- task_08/src/test.cpp
+++ task_08/src/test.cpp
@@ -7,31 +7,31 @@
 
 TEST(QuickSelect, ExamplesFromReadme) {
   std::vector<int> a1 = {7, 10, 4, 3, 20};
-  EXPECT_EQ(quick_selest(5, 3, a1), 7);
+  EXPECT_EQ(7, QuickSelect(3, a1));
 
   std::vector<int> a2 = {7, 10, 4, 3, 20};
-  EXPECT_EQ(quick_selest(5, 1, a2), 3);
+  EXPECT_EQ(3, QuickSelect(1, a2));
 
   std::vector<int> a3 = {7, 10, 4, 3, 20};
-  EXPECT_EQ(quick_selest(5, 5, a3), 20);
+  EXPECT_EQ(20, QuickSelect(5, a3));
 }
 
 TEST(QuickSelect, DuplicatesAndNegatives) {
   std::vector<int> v = {5, -1, 5, 2, 2, -1};
   // Sorted: -1, -1, 2, 2, 5, 5
-  EXPECT_EQ(quick_selest(6, 1, v), -1);
-  EXPECT_EQ(quick_selest(6, 2, v), -1);
-  EXPECT_EQ(quick_selest(6, 3, v), 2);
-  EXPECT_EQ(quick_selest(6, 6, v), 5);
+  EXPECT_EQ(-1, QuickSelect(1, v));
+  EXPECT_EQ(-1, QuickSelect(2, v));
+  EXPECT_EQ(2, QuickSelect(3, v));
+  EXPECT_EQ(5, QuickSelect(6, v));
 }
 
 TEST(QuickSelect, SingleElement) {
   std::vector<int> v = {42};
-  EXPECT_EQ(quick_selest(1, 1, v), 42);
+  EXPECT_EQ(42, QuickSelect(1, v));
 }
 
 TEST(QuickSelect, AlreadySorted) {
   std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  EXPECT_EQ(quick_selest(9, 4, v), 4);
-  EXPECT_EQ(quick_selest(9, 9, v), 9);
+  EXPECT_EQ(4, QuickSelect(4, v));
+  EXPECT_EQ(9, QuickSelect(9, v));
 }
